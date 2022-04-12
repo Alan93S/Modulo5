9a727039c00119a901adba52958f93c60ae627a6
Cesar <camongez28@gmail.com>
2022-04-12 00:04:08 -0400

    estilos e impresion de cards

--- script.js
+++ script.js
@@ -18,16 +18,32 @@ function traerPokemons(number){
 // EL NUMERO NOMBRE E IMAGEN DEL POKEMON
 function tabla(datos) {
         
-    contenido.innerHTML += `                
-            <tr>
+    // contenido.innerHTML += `                
+    //         <tr>
             
-                <td> <img src=' ${ datos.sprites.front_default }'/></td>
-                <th scope="row">${ datos.name }</th>
-                <td>${ datos.id }</td>
+    //             <td> <img src=' ${ datos.sprites.front_default }'/></td>
+    //             <th scope="row">$datos.name</th>
+    //             <td>${ datos.id }</td>
 
-            </tr>
+    //         </tr>
                 
-            `
+    //         `
+
+    contenido.innerHTML += `
+    <div class="card-father col-12 col-md-6 col-lg-4 mb-3">
+				<div class="card d-flex flex-row shadow rounded">
+					<img src="${ datos.sprites.front_default}" class="card__img img-fluid" alt="">
+					<div class="card-body d-flex justify-content-center align-items-center p-0">
+                    <h2 class="card-title h5 m-1">${datos.name}</h2>
+					<p class="h5 m-0">#${datos.id}</p>
+					</div>
+					<div class="d-flex flex-column justify-content-center p-2">
+						<a href="#" class="btn btn-primary card__button mb-1">Detalles</a>
+						<a href="#" class="btn btn-danger card__button">Favorito</a>
+					</div>
+				</div>
+			</div>
+    `
 }
 
 // SE LLAMA LA FUNCION Y SE INDICA CUANTOS DATOS SE MOSTRARA EN LA PANTALLA, EN ESTE CASO 10, SI SE CAMBIA SE MUESTRAN MAS O MENOS
