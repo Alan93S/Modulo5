ffea95d2d8685268e81f387c9197115faad88646
Daniel <adrecolin@gmail.com>
2022-04-12 13:04:16 -0400

    agregado anterior y siguiente
    ya no es necesario agregar manualmente el numero para ver cuantos  ya que con el boton siguiente o anterior mostrara los proximos

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-let contenido = document.querySelector('.contenido')
+const contenido = document.querySelector('.contenido')
+const previous = document.querySelector("#previous");
+const next = document.querySelector("#next");
+
+// MUESTRA EL LIMITE DE POKEMOS POR PANTALLA
+let offset = 1;
+let limit = 8;
+
+// EVENTO DE AL CLICKEAR ANTERIOR MUESTRE LOS 9 ANTERIORES A MENOS QUE ESTE EN LA PRIMERA PAGINA 
+
+previous.addEventListener("click", () => {
+    if (offset != 1) {
+      offset -= 9;
+      removeChildNodes(contenido);
+      traerPokemons(offset, limit);
+    }
+  });
+
+  // EVENTO DE AL CLICKEAR SIGUIENTE MUESTRE LOS 9 SIGUIENTESA 
+
+  next.addEventListener("click", () => {
+    offset += 9;
+    removeChildNodes(contenido);
+    traerPokemons(offset, limit);
+  });
+    
 
 function traerPokemon(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
@@ -9,8 +34,8 @@ function traerPokemon(id) {
 
 }
 // FUNCION PARA QUE NOS MUESTRE UNA CANTIDAD ESPECIFICA DE POKEMONES
-function traerPokemons(number){
-    for (let i = 1; i <= number; i++){
+function traerPokemons(offset, limit){
+    for (let i = offset; i <= offset + limit; i++){
          traerPokemon(i);
     }
 }
@@ -18,31 +43,21 @@ function traerPokemons(number){
 // EL NUMERO NOMBRE E IMAGEN DEL POKEMON
 function tabla(datos) {
         
-    // contenido.innerHTML += `                
-    //         <tr>
-            
-    //             <td> <img src=' ${ datos.sprites.front_default }'/></td>
-    //             <th scope="row">$datos.name</th>
-    //             <td>${ datos.id }</td>
-
-    //         </tr>
-                
-    //         `
 
     contenido.innerHTML += `
     <div class="card-father col-12 col-md-6 col-lg-4 mb-3 ">
-				<div class="card d-flex flex-row shadow rounded">
-					<img src="${ datos.sprites.front_default}" class="card__img img-fluid" alt="">
-					<div class="card-body d-flex justify-content-center flex-column align-items-center p-0">
+                <div class="card d-flex flex-row shadow rounded">
+                    <img src="${ datos.sprites.front_default}" class="card__img img-fluid" alt="">
+                    <div class="card-body d-flex justify-content-center flex-column align-items-center p-0">
                     <p class="h5 m-0">#${datos.id}</p>
                     <h2 class="card-title h5 m-1">${datos.name}</h2>
-					</div>
-					<div class="d-flex flex-column justify-content-center p-2">
-						<a href="#" class="btn btn-primary h6 card__button mb-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Más detalles">Detalles</a>
-						<a href="#" class="btn btn-danger card__button" data-bs-toggle="tooltip" data-bs-placement="top" title="Añadir a favoritos">Favorito</a>
-					</div>
-				</div>
-			</div>
+                    </div>
+                    <div class="d-flex flex-column justify-content-center p-2">
+                        <a href="#" class="btn btn-primary h6 card__button mb-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Más detalles">Detalles</a>
+                        <a href="#" class="btn btn-danger card__button" data-bs-toggle="tooltip" data-bs-placement="top" title="Añadir a favoritos">Favorito</a>
+                    </div>
+                </div>
+            </div>
     `
 
     
@@ -50,4 +65,15 @@ function tabla(datos) {
 
 // SE LLAMA LA FUNCION Y SE INDICA CUANTOS DATOS SE MOSTRARA EN LA PANTALLA, EN ESTE CASO 10, SI SE CAMBIA SE MUESTRAN MAS O MENOS
 
-traerPokemons(10);
+
+// EVENTO QUE HACER REMOVER LOS PRIMEROS 9 CARDS Y NO SE VAYAN ACUMULANDO DENTRO DE LA MISMA Y VICEVERSA
+
+function removeChildNodes(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
+  
+  
+
+traerPokemons(offset, limit);
