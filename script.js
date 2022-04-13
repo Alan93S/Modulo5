287949ece2594dfcc361512260599c7a839dcf76
Alan S <a.sanmartin.correa@gmail.com>
2022-04-13 11:23:45 -0400

    se agrega modal

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function traerPokemons(number){
          traerPokemon(i);
     }
 }
+
 // ACA ES DONDE SE ARMA LA ESTRUCTURA, datos ES EL NOMBRE QUE LE DAMOS, SPRITES, NAME E ID DE LA LINEA 23 A 25 SON DATOS EXCLUSIVOS DE LA API QUE NOS DAN
 // EL NUMERO NOMBRE E IMAGEN DEL POKEMON
 let pokemones = [];
@@ -28,12 +29,41 @@ function tabla(datos) {
                     <p class="h5 m-0">#${datos.id}</p>
                     <h2 class="card-title h5 m-1">${datos.name}</h2>
 					</div>
+        
 					<div class="d-flex flex-column justify-content-center p-2">
-						<a href="#" class="btn btn-primary h6 card__button mb-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Más detalles">Detalles</a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${datos.id}">
+                    Detalles
+                  </button>
+					
 						<a href="#" class="btn btn-danger card__button" data-bs-toggle="tooltip" data-bs-placement="top" title="Añadir a favoritos" onclick="agregarFavorito(${datos.id})">Favorito</a>
 					</div>
 				</div>
 			</div>
+            <div class="modal fade" id="exampleModal${datos.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  
+               
+                <img src="${ datos.sprites.front_default}" class="card__img img-fluid" alt="">
+                <div class="card-body d-flex justify-content-center flex-column align-items-center p-0">
+                <p class="h5 m-0">#${datos.id}</p>
+                <p class="h5 m-0">#${datos.weight}</p>
+                <h2 class="card-title h5 m-1">${datos.name}</h2>
+                </div>
+                    
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
     `
 }
 
@@ -86,11 +116,14 @@ btnSearch.addEventListener("click", (e) => {
 
    const textSearch = inputSearch.value
 
+
    const newPokemones = pokemones.filter(function(pokemon){
 
     if(pokemon.name.includes(`${textSearch}`)) {
 
         return  pokemon 
+
+        
     }
 
     
